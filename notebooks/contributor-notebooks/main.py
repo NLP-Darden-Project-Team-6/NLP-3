4adcb5225cc83e8bda1956c58d1b8a6c8a2669b5
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-11-20 12:21:56 -0600

    updates

--- notebooks/contributor-notebooks/main.py
+++ notebooks/contributor-notebooks/main.py
@@ -1,4 +1,5 @@
 import sys
 
-sys.path.insert(1, '../../data/')
-sys.path.insert(2, '../../src/')
+sys.path.insert(1, '../../data/raw/')
+sys.path.insert(2, '../../data/prepared/')
+sys.path.insert(3, '../../src/')
